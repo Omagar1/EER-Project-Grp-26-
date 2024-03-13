018f98781fbd288e3e7827363437f950aef7d8e7
Omagar1 <josiah.rowden@gmail.com>
2024-03-13 11:56:31 +0000

    fixing mistakes with DB stuff

--- DBStructure.sql
+++ DBStructure.sql
@@ -1,5 +1,5 @@
 
-
+DROP TABLE IF EXISTS account;
 CREATE TABLE account (
     accountID INTEGER PRIMARY KEY AUTO_INCREMENT,
     emailAddress VARCHAR(255) NOT NULL,
@@ -9,30 +9,31 @@ CREATE TABLE account (
     `role`VARCHAR(255) NOT NULL,
     active BOOLEAN NOT NULL DEFAULT true
 );
-
+DROP TABLE IF EXISTS property;
 CREATE TABLE property (
     propertyID INTEGER PRIMARY KEY AUTO_INCREMENT,
     ownerID INTEGER NOT NULL,
     EER CHAR NOT NULL,
     `postcode` VARCHAR(255) NOT NULL, -- Presumptive
     `address` VARCHAR(255) NOT NULL, -- Presumptive
-    `reportIssueDate `date NOT NULL, -- Presumptive
+    `reportIssueDate` DATE NOT NULL, -- Presumptive
     certificateNumber VARCHAR(255) NOT NULL -- Presumptive
 );
-
+DROP TABLE IF EXISTS equation;
 CREATE TABLE equation (
     equationID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    equation VARCHAR(255) NOT NULL, 
+    equation VARCHAR(255) NOT NULL
 );
-
+DROP TABLE IF EXISTS userSavedProperty;
 CREATE TABLE userSavedProperty (
-    userID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    propertyID INTEGER PRIMARY KEY AUTO_INCREMENT
+    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    userID INTEGER,
+    propertyID INTEGER
 );
-
+DROP TABLE IF EXISTS recommendation;
 CREATE TABLE recommendation ( 
     recommendationID INTEGER PRIMARY KEY AUTO_INCREMENT, 
     propertyID INTEGER NOT NULL, 
     `carbonEmistionsImpact` VARCHAR(255) NOT NULL, 
-    `recomendedChanges` VARCHAR(255) NOT NULL, 
+    `recomendedChanges` VARCHAR(255) NOT NULL
 );
--- dbConnect.php
+++ dbConnect.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
-$username = "";
-$password = "";
+$username = "EER";
+$password = "1234";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=tms; charset=utf8mb4", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=eerDB; charset=utf8mb4", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully </br>"; //test
