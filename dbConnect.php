2e46fee30b98f8d54065e5d5f5084280a326dc61
gianniw11 <giannidionwatkinson@outlook.com>
2024-03-15 12:01:15 +0000

    Added Login and Create Account functionality

--- dbConnect.php
+++ dbConnect.php
@@ -4,7 +4,7 @@ $username = "EER";
 $password = "1234";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=eerDB; charset=utf8mb4", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=eerdb; charset=utf8mb4", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully </br>"; //test
