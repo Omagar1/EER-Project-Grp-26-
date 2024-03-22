3702c30b4dcc5225fd44429e237fd10daa5ea14f
Omagar1 <josiah.rowden@gmail.com>
2024-03-22 10:47:33 +0000

    to fix timeout issue

--- dbConnect.php
+++ dbConnect.php
@@ -6,13 +6,24 @@ $password = "ER4sever!"; //ER4sever!
 $dbname = "eer-db"; 
 //sqlsrv:server = tcp:eer-sever.database.windows.net,1433
 //$conn = new PDO("sqlsrv:server = tcp:eer-sever.database.windows.net,1433; Database = eer-db", "EER-admin", "{your_password_here}");
-try {
-  $conn = new PDO("sqlsrv:server=$servername; Database=$dbname;", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully </br>"; //test
-} catch(PDOException $e) {
-  echo "Error: " . $e; // dispable after development
-  echo "Connection failed"; 
-}
+$count = 0; 
+$connectionMade = false;
+$errorLog;  
+do{
+  $count++; 
+  try {
+    $conn = new PDO("sqlsrv:server=$servername; Database=$dbname;", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connectionMade = true;
+    //echo "Connected successfully </br>"; //test
+  } catch(PDOException $e) {
+    $errorLog["DBconnect".$count] = "Error: " . $e; 
+    //echo "Error: " . $e; // dispable after development
+    //echo "Connection failed"; 
+  }
+
+}while($connectionMade Or $count == 5);
+  
+
 ?>
