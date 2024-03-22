5f610b06ed3b2837874cead0542d1428eed9032a
sebtarabadi <starabadi172@gmail.com>
2024-03-22 12:39:41 +0000

    added top nav and css

---
+++ navBar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>View User Accounts</title>
+        <link rel="stylesheet" href="styles.css"/>
+    </head>
+
+<body>
+
+<header class="header">
+
+<div class="header-text">
+<a href="home.php">Energy Efficiency Application</a>
+</div>
+
+<div class="header-buttons">
+    <a href="placeholder.php">Manage Properties</a>
+    <a href="placeholder.php">Calculate EER</a>
+    <a href="placeholder.php">View Profile</a>
+    <a href="placeholder.php">Log In</a>
+</div>
+
+</header>
+
+<p>body</p>
+
+</body>
+</html>
