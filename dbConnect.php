5ddde3b8f213dd038dcc1e318acc60470213d63f
Omagar1 <josiah.rowden@gmail.com>
2024-03-22 10:49:59 +0000

    a better solution to previous

--- dbConnect.php
+++ dbConnect.php
@@ -6,24 +6,16 @@ $password = "ER4sever!"; //ER4sever!
 $dbname = "eer-db"; 
 //sqlsrv:server = tcp:eer-sever.database.windows.net,1433
 //$conn = new PDO("sqlsrv:server = tcp:eer-sever.database.windows.net,1433; Database = eer-db", "EER-admin", "{your_password_here}");
-$count = 0; 
-$connectionMade = false;
-$errorLog;  
-do{
-  $count++; 
-  try {
-    $conn = new PDO("sqlsrv:server=$servername; Database=$dbname;", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connectionMade = true;
-    //echo "Connected successfully </br>"; //test
-  } catch(PDOException $e) {
-    $errorLog["DBconnect".$count] = "Error: " . $e; 
-    //echo "Error: " . $e; // dispable after development
-    //echo "Connection failed"; 
-  }
-
-}while($connectionMade Or $count == 5);
-  
-
+try {
+  $conn = new PDO("sqlsrv:server=$servername; Database=$dbname;", $username, $password, array(
+    PDO::ATTR_TIMEOUT => 5, // in seconds
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+));
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully </br>"; //test
+} catch(PDOException $e) {
+  echo "Error: " . $e; // dispable after development
+  echo "Connection failed"; 
+}
 ?>
