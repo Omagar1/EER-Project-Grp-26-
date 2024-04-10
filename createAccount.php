11566cb0a198e4582d204f0810451ce693edb828
Omagar1 <josiah.rowden@gmail.com>
2024-04-10 12:34:47 +0100

    .

--- createAccount.php
+++ createAccount.php
@@ -28,9 +28,9 @@ function createAccount($conn, $email, $hashPassword, $role)
             
             $stmt1->bindParam(':role', $role, PDO::PARAM_STR);
             if($role == "admin"){
-                $stmt1->bindParam(':active', (bool)1 , PDO::PARAM_BOOL);// as admin accounts should be aproved by admins first -stoped for testing
+                $stmt1->bindParam(':active', (int)1 , PDO::PARAM_INT);// as admin accounts should be aproved by admins first -stoped for testing
             }else{
-                $stmt1->bindParam(':active', (bool)1 , PDO::PARAM_BOOL);
+                $stmt1->bindParam(':active', (int)1 , PDO::PARAM_INT);
             }
             
             $stmt1->execute();
