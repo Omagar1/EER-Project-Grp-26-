a5b333726c86015cd1fe0727bdc1fd9c6ab97d1a
sebtarabadi <starabadi172@gmail.com>
2024-04-11 22:14:56 +0100

    css

--- deleteUser.php
+++ deleteUser.php
@@ -24,7 +24,7 @@ try{
         <link rel="stylesheet" href="styles.css"/>
     </head>
     <body>
-        <div class="container">
+        <div class="delete-container">
             <h2>Delete User <?php echo $_GET['id'];?>?</h2>
         </div>
         <div class="container">
