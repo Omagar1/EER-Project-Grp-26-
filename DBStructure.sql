b87f83920d11223be3f9840f72efcacda24dce08
gianniw11 <giannidionwatkinson@outlook.com>
2024-04-12 11:29:14 +0100

    local and azure now both in same files + addProperty.php

--- DBStructure.sql
+++ DBStructure.sql
@@ -1,45 +1,44 @@
 
 DROP TABLE IF EXISTS account;
 CREATE TABLE account (
-    accountID INTEGER PRIMARY KEY IDENTITY(1,1),
+    accountID INTEGER PRIMARY KEY AUTO_INCREMENT,
     emailAddress VARCHAR(255) NOT NULL,
-    verifyEmail BIT NOT NULL DEFAULT 0,
-    emailChanged DATETIME NOT NULL DEFAULT GETDATE(), -- when first created is the same time as date created
-    emailChangedBy INTEGER NOT NULL DEFAULT 0, -- foreign key refference account ID
-    password VARCHAR(255) NOT NULL,
-    dateCreated DATE NOT NULL DEFAULT GETDATE(),
-    role VARCHAR(255) NOT NULL,
-    active INTEGER NOT NULL DEFAULT 0
+    verifyEmail BOOLEAN NOT NULL DEFAULT false,
+    emailChanged DATETIME NOT NULL, -- when first created is the same time as date created
+    emailChangedBy INTEGER NOT NULL, -- foreign key refference account ID
+    `password` VARCHAR(255) NOT NULL,
+    dateCreated DATE NOT NULL,
+    `role`VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT true
 );
 DROP TABLE IF EXISTS property;
 CREATE TABLE property (
-    propertyID INTEGER PRIMARY KEY IDENTITY(1,1),
+    propertyID INTEGER PRIMARY KEY AUTO_INCREMENT,
     ownerID INTEGER NOT NULL, -- foreign key refference account ID
     EER CHAR NOT NULL, 
-    postcode VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL, 
-    addressChanged DATETIME NOT NULL DEFAULT GETDATE(), -- when first created is the same time as date created
-    addressChangedBy INTEGER NOT NULL, -- foreign key refference account ID
-    reportIssueDate DATE NOT NULL,
+    `postcode` VARCHAR(255) NOT NULL, -- Presumptive
+    `address` VARCHAR(255) NOT NULL, -- Presumptive
+    addressChanged DATETIME NOT NULL, -- when first created is the same time as date created
+    postcodeChangedBy INTEGER NOT NULL, -- foreign key refference account ID
+    `reportIssueDate` DATE NOT NULL, -- Presumptive
+    certificateNumber VARCHAR(255) NOT NULL -- Presumptive
 );
-
+DROP TABLE IF EXISTS equation;
+CREATE TABLE equation (
+    equationID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    equation VARCHAR(255) NOT NULL
+);
 DROP TABLE IF EXISTS userSavedProperty;
 CREATE TABLE userSavedProperty (
-    ID INTEGER PRIMARY KEY IDENTITY(1,1),
+    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
     userID INTEGER, -- foreign key refference account ID
     propertyID INTEGER -- foreign key refference property ID
 );
 DROP TABLE IF EXISTS recommendation;
 CREATE TABLE recommendation ( 
-    recommendationID INTEGER PRIMARY KEY IDENTITY(1,1), 
+    recommendationID INTEGER PRIMARY KEY AUTO_INCREMENT, 
     propertyID INTEGER NOT NULL, -- foreign key refference property ID 
-    carbonEmistionsImpact VARCHAR NOT NULL, 
-    recomendedChanges VARCHAR(255) NOT NULL
+    `carbonEmistionsImpact` VARCHAR(255) NOT NULL, 
+    `recomendedChanges` VARCHAR(255) NOT NULL
 );
-
--- DROP TABLE IF EXISTS equation;
--- CREATE TABLE equation (
---     equationID INTEGER PRIMARY KEY IDENTITY(1,1),
---     equation VARCHAR NOT NULL
--- );
  
---
+++ property.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+require_once 'dbConnect.php';
+require_once 'calc.php';
+require_once 'addProperty.php';
+
+$msg = "";
+
+if (isset($_POST['submit']))
+{
+    $msg = addProperty($conn, $_SESSION['userID'], EERCalc($_POST['tfa'],$_POST['lc'],$_POST['hc'], $_POST['hwc']), $_POST['postcode'], $_POST['address']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Property</title>
+</head>
+<body>
+    <form action="property.php" method="post">
+        <label for="postcode">Postcode</label><br>
+        <input type="text" name="postcode" required><br><br>
+        
+        <label for="address">Address</label><br>
+        <input type="text" name="address" required><br><br>
+
+        <label for="tfa">Total Floor Area</label><br>
+        <input type="text" name="tfa"><br><br>
+
+        <label for="lc">Lighting Cost</label><br>
+        <input type="text" name="lc"><br><br>
+        
+        <label for="hc">Heating Cost</label><br>
+        <input type="text" name="hc"><br><br>
+        
+        <label for="hwc">Hot Water Cost</label><br>
+        <input type="text" name="hwc"><br><br>
+        
+        <input type="submit" name="submit"><br><br>
+        <div>
+            <?php echo $msg; ?>
+        </div>
+
+    </form>
+    
+</body>
+</html>
