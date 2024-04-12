1683654603218f8d0d772432a2301909ebc4e1d8
gianniw11 <giannidionwatkinson@outlook.com>
2024-04-12 11:37:04 +0100

    Revert "local and azure now both in same files + addProperty.php"
    This reverts commit b87f83920d11223be3f9840f72efcacda24dce08.

--- DBStructure.sql
+++ DBStructure.sql
@@ -1,44 +1,45 @@
 
 DROP TABLE IF EXISTS account;
 CREATE TABLE account (
-    accountID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    accountID INTEGER PRIMARY KEY IDENTITY(1,1),
     emailAddress VARCHAR(255) NOT NULL,
-    verifyEmail BOOLEAN NOT NULL DEFAULT false,
-    emailChanged DATETIME NOT NULL, -- when first created is the same time as date created
-    emailChangedBy INTEGER NOT NULL, -- foreign key refference account ID
-    `password` VARCHAR(255) NOT NULL,
-    dateCreated DATE NOT NULL,
-    `role`VARCHAR(255) NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT true
+    verifyEmail BIT NOT NULL DEFAULT 0,
+    emailChanged DATETIME NOT NULL DEFAULT GETDATE(), -- when first created is the same time as date created
+    emailChangedBy INTEGER NOT NULL DEFAULT 0, -- foreign key refference account ID
+    password VARCHAR(255) NOT NULL,
+    dateCreated DATE NOT NULL DEFAULT GETDATE(),
+    role VARCHAR(255) NOT NULL,
+    active INTEGER NOT NULL DEFAULT 0
 );
 DROP TABLE IF EXISTS property;
 CREATE TABLE property (
-    propertyID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    propertyID INTEGER PRIMARY KEY IDENTITY(1,1),
     ownerID INTEGER NOT NULL, -- foreign key refference account ID
     EER CHAR NOT NULL, 
-    `postcode` VARCHAR(255) NOT NULL, -- Presumptive
-    `address` VARCHAR(255) NOT NULL, -- Presumptive
-    addressChanged DATETIME NOT NULL, -- when first created is the same time as date created
-    postcodeChangedBy INTEGER NOT NULL, -- foreign key refference account ID
-    `reportIssueDate` DATE NOT NULL, -- Presumptive
-    certificateNumber VARCHAR(255) NOT NULL -- Presumptive
+    postcode VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL, 
+    addressChanged DATETIME NOT NULL DEFAULT GETDATE(), -- when first created is the same time as date created
+    addressChangedBy INTEGER NOT NULL, -- foreign key refference account ID
+    reportIssueDate DATE NOT NULL,
 );
-DROP TABLE IF EXISTS equation;
-CREATE TABLE equation (
-    equationID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    equation VARCHAR(255) NOT NULL
-);
+
 DROP TABLE IF EXISTS userSavedProperty;
 CREATE TABLE userSavedProperty (
-    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    ID INTEGER PRIMARY KEY IDENTITY(1,1),
     userID INTEGER, -- foreign key refference account ID
     propertyID INTEGER -- foreign key refference property ID
 );
 DROP TABLE IF EXISTS recommendation;
 CREATE TABLE recommendation ( 
-    recommendationID INTEGER PRIMARY KEY AUTO_INCREMENT, 
+    recommendationID INTEGER PRIMARY KEY IDENTITY(1,1), 
     propertyID INTEGER NOT NULL, -- foreign key refference property ID 
-    `carbonEmistionsImpact` VARCHAR(255) NOT NULL, 
-    `recomendedChanges` VARCHAR(255) NOT NULL
+    carbonEmistionsImpact VARCHAR NOT NULL, 
+    recomendedChanges VARCHAR(255) NOT NULL
 );
+
+-- DROP TABLE IF EXISTS equation;
+-- CREATE TABLE equation (
+--     equationID INTEGER PRIMARY KEY IDENTITY(1,1),
+--     equation VARCHAR NOT NULL
+-- );
  
