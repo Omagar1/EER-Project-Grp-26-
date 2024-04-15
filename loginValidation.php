6366204256e7da99cc6ca7c4573c3d54cf514b28
Omagar1 <josiah.rowden@gmail.com>
2024-04-15 10:57:36 +0100

    login fix

--- loginValidation.php
+++ loginValidation.php
@@ -14,7 +14,7 @@ function loginValidation($conn, $email, $hashPassWord) {
 
         if ($result)
         {
-            $_SESSION['userID'] = $result['ID'];
+            $_SESSION['userID'] = $result['accountID'];
             $_SESSION['userRole'] = $result['role'];
             $_SESSION['username'] = str_replace("."," ",explode("@",$result['email'])[0]);
             $_SESSION["loggedIn"] = true;
