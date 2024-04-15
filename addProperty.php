7ad6bc72c755f3ca720b3b1fbe873bcdb0246421
Omagar1 <josiah.rowden@gmail.com>
2024-04-15 11:04:48 +0100

    added more columns to insert stmt

--- addProperty.php
+++ addProperty.php
@@ -6,7 +6,6 @@ function addProperty($conn, $ownerID, $EER, $postcode, $address)
         $currentDate = date('Y-m-d');
 
         $sql1 = "SELECT * FROM property WHERE address = :address AND postcode = :postcode;";
-        $sql2 = "INSERT INTO property (ownerID, EER, postcode, address, reportIssueDate) VALUES (:ownerID, :EER, :postcode, :address, :reportIssueDate)";
 
         $stmt1 = $conn->prepare($sql1);
 
@@ -27,11 +26,15 @@ function addProperty($conn, $ownerID, $EER, $postcode, $address)
             return "Property already exists.";
         } else 
         {
+            $sql2 = "INSERT INTO property (ownerID, EER, postcode, address, addressChanged, addressChangedBy, reportIssueDate) VALUES (:ownerID, :EER, :postcode, :address, :addressChanged, :addressChangedBy, :reportIssueDate)";
+
             $stmt2 = $conn->prepare($sql2);
-            $stmt2->bindParam(':ownerID', $ownerID, PDO::PARAM_STR);
+            $stmt2->bindParam(':ownerID', $ownerID, PDO::PARAM_INT);
             $stmt2->bindParam(':EER', $EER, PDO::PARAM_STR);
             $stmt2->bindParam(':postcode', $postcode, PDO::PARAM_STR);
             $stmt2->bindParam(':address', $address, PDO::PARAM_STR);
+            $stmt2->bindParam(':addressChanged', $currentDate, PDO::PARAM_INT);
+            $stmt2->bindParam(':addressChangedBy', $ownerID, PDO::PARAM_INT);
             $stmt2->bindParam(':reportIssueDate', $currentDate, PDO::PARAM_INT);
             $stmt2->execute();
 
