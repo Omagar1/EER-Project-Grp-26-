3a36ffd26bec4b8c6783aa21f5400ef4e728f7cb
FT6374 <162442615+FT6374@users.noreply.github.com>
2024-04-17 11:27:09 +0100

    .

--- saveProperty.php
+++ saveProperty.php
@@ -4,7 +4,6 @@ session_start([
     'gc_maxlifetime' => 3600, 
    ]);
 require("dbConnect.php");
-require_once "notLoggedIn.php";
 $userid = $_SESSION["userID"];
 try{
     $sql1 = "SELECT userID,propertyID FROM userSavedProperty WHERE userID=:uid,propertyID=:pid";
@@ -12,12 +11,18 @@ try{
     $result->bindParam(':pid', $_REQUEST['pid'], PDO::PARAM_INT);
     $result->bindParam(':uid', $userid, PDO::PARAM_INT);
     $result->execute();
-    if (isset($_POST['save']) && $result->rowCount() == 0) {
-        $sql = "INSERT INTO userSavedProperty (userID,propertyID)VALUES(:uid,:pid)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':pid', $_REQUEST['pid'], PDO::PARAM_INT);
-        $stmt->bindParam(':uid', $userid, PDO::PARAM_INT);
-        $stmt->execute();
+    if ($result->rowCount() == 0) {
+        if (isset($_POST['save'])) {
+            $sql = "INSERT INTO userSavedProperty (userID,propertyID)VALUES(:uid,:pid)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':pid', $_REQUEST['pid'], PDO::PARAM_INT);
+            $stmt->bindParam(':uid', $userid, PDO::PARAM_INT);
+            $stmt->execute();
+        }
+    }else{
+        if (isset($_POST['save'])) {
+            echo"Already saved";
+        }
     }
     header("location: userViewProperty.php");
 }catch(PDOException $e){
