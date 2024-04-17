5766d9a91303f163beb1320af1300a02390ee8e5
sebtarabadi <starabadi172@gmail.com>
2024-04-17 12:38:41 +0100

    css

--- userViewProperty.php
+++ userViewProperty.php
@@ -100,9 +100,7 @@ $userid = $_SESSION["userID"];
                     $stmt->execute();
                         
                         while($row= $stmt->fetch(PDO::FETCH_ASSOC)){
-                            if (isset($row)){
-                                 var_dump($row['EER']);
-                            
+                            if (isset($row)){                            
                         ?>
                         <div class="<?php echo ratingTocolour($row["EER"])?>">
                             <div>
