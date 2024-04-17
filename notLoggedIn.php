7cd8bfe6d96b3f69af1dbf13088b999e7c9544ae
Omagar1 <josiah.rowden@gmail.com>
2024-04-17 12:18:37 +0100

    now logs you out

--- notLoggedIn.php
+++ notLoggedIn.php
@@ -3,6 +3,6 @@
 if (!isset($_SESSION["loggedIn"]) and ($_SESSION["loggedIn"] != true)) {
     echo "you should have been logged out bc loggedIn = ".$_SESSION["loggedIn"]."  ";
     
-    //header("location: logout.php"); // if so redirects the user  to the logout page as to unset any sessions they may have set already. 
+    header("location: logout.php"); // if so redirects the user  to the logout page as to unset any sessions they may have set already. 
 }
 ?>
