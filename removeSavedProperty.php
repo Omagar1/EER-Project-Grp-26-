655e58f9d955c3777cb482b6601e21152ec05a00
FT6374 <162442615+FT6374@users.noreply.github.com>
2024-04-19 10:32:10 +0100

    .

--- removeSavedProperty.php
+++ removeSavedProperty.php
@@ -7,13 +7,13 @@ session_start([
 require("dbConnect.php");
 $userid = $_SESSION["userID"];
 try{
-    if (isset($_POST['unsave'])) {
+    //if (isset($_POST['unsave'])) {
         $sql = "DELETE FROM userSavedProperty WHERE userID=:uid AND propertyID = :pid";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':uid', $userid, PDO::PARAM_INT);
         $stmt->bindParam(':pid', $_GET['pid'], PDO::PARAM_INT);
         $stmt->execute();
-    }
+    //}
     header("location: viewSavedProperty.php");
 }catch(PDOException $e){
     echo $e;
