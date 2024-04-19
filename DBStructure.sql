1d8d94e38f3d4b7adf0bdb91201113d32f25dbeb
Omagar1 <josiah.rowden@gmail.com>
2024-04-19 11:29:05 +0100

    db structure change

--- DBStructure.sql
+++ DBStructure.sql
@@ -5,7 +5,7 @@ CREATE TABLE account (
     emailAddress VARCHAR(255) NOT NULL,
     verifyEmail BIT NOT NULL DEFAULT 0,
     emailChanged DATETIME NOT NULL DEFAULT GETDATE(), -- when first created is the same time as date created
-    emailChangedBy INTEGER NOT NULL DEFAULT 0, -- foreign key refference account ID
+    emailChangedBy INTEGER NOT NULL DEFAULT 0  FOREIGN KEY REFERENCES account(accountID),
     password VARCHAR(255) NOT NULL,
     dateCreated DATE NOT NULL DEFAULT GETDATE(),
     role VARCHAR(255) NOT NULL,
@@ -14,29 +14,29 @@ CREATE TABLE account (
 DROP TABLE IF EXISTS property;
 CREATE TABLE property (
     propertyID INTEGER PRIMARY KEY IDENTITY(1,1),
-    ownerID INTEGER NOT NULL, -- foreign key refference account ID
+    ownerID INTEGER NOT NULL FOREIGN KEY REFERENCES account(accountID) ON DELETE CASCADE,
     EER CHAR NOT NULL, 
     postcode VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     propertyType VARCHAR(255) NOT NULL,
     addressChanged DATETIME NOT NULL DEFAULT GETDATE(), -- when first created is the same time as date created
-    addressChangedBy INTEGER NOT NULL, -- foreign key refference account ID
-    reportIssueDate DATE NOT NULL,
+    addressChangedBy INTEGER NOT NULL FOREIGN KEY REFERENCES account(accountID),
+    reportIssueDate DATE NOT NULL
 );
 
 DROP TABLE IF EXISTS userSavedProperty;
 CREATE TABLE userSavedProperty (
     ID INTEGER PRIMARY KEY IDENTITY(1,1),
-    userID INTEGER, -- foreign key refference account ID
-    propertyID INTEGER -- foreign key refference property ID
+    userID INTEGER FOREIGN KEY REFERENCES account(accountID) ON DELETE CASCADE, 
+    propertyID INTEGER FOREIGN KEY REFERENCES account(propertyID) ON DELETE CASCADE 
 );
-DROP TABLE IF EXISTS recommendation;
-CREATE TABLE recommendation ( 
-    recommendationID INTEGER PRIMARY KEY IDENTITY(1,1), 
-    propertyID INTEGER NOT NULL, -- foreign key refference property ID 
-    carbonEmistionsImpact VARCHAR NOT NULL, 
-    recomendedChanges VARCHAR(255) NOT NULL
-);
+-- DROP TABLE IF EXISTS recommendation;
+-- CREATE TABLE recommendation ( 
+--     recommendationID INTEGER PRIMARY KEY IDENTITY(1,1), 
+--     propertyID INTEGER NOT NULL, -- foreign key refference property ID 
+--     carbonEmistionsImpact VARCHAR NOT NULL, 
+--     recomendedChanges VARCHAR(255) NOT NULL
+-- );
 
 -- DROP TABLE IF EXISTS equation;
 -- CREATE TABLE equation (
